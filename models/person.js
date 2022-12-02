cd4a7de92e21cf9a62986f4976b4b3f85b7d38de
fallmx <31561277+fallmx@users.noreply.github.com>
2022-12-02 21:44:31 +0200

    Teht 3.19-3.21

--- models/person.js
+++ models/person.js
@@ -12,8 +12,20 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /(\d{2}-\d{5,})|(\d{3}-\d{4,})/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
